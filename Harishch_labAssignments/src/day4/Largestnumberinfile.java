f12bd1f1b46975064ecb03d7078d32680ee53637
unknown <mrhypothetic23@gmail.com>
2022-03-23 20:24:57 +0530

    updated

--- Harishch_labAssignments/src/day4/Largestnumberinfile.java
+++ Harishch_labAssignments/src/day4/Largestnumberinfile.java
@@ -1,29 +1,49 @@
 package day4;
 
+ import java.util.ArrayList;
+//import java.util.List;
+//import java.util.Scanner;
+//import java.nio.Buffer;
+//import java.nio.file.Files;
 import java.io.*;
-import java.util.*;
+public class Largestnumberinfile {
+	public static void main(String args[]) throws IOException
+	{
+		try
+		{
+			BufferedReader br=new BufferedReader(new FileReader("data.txt"));
+			ArrayList<Double>arr=new ArrayList<>();
+			String line;
+			while((line=br.readLine())!=null)
+			{
+				String[] r=line.split(" ");
+				for(int i=0;i<r.length;i++)
+				{
+					double val=Double.parseDouble(r[i]);
+					arr.add(val);
+					System.out.println(val);
+				}
+			}
+			br.close();
+			double max=0;
+			for(int i=0;i<arr.size();i++)
+			{
+	           if(arr.get(i)>max)
+	           {
+	        	   max=arr.get(i);
+	           }
+			}
+			System.out.println("the maximum double value is " +max);
+	}
+		catch(IOException io)
+		{
+			System.out.println("Error occurred");
+		}
+		catch(NumberFormatException n)
+		{
+			System.out.println("Error occurred");
+		}
 
-class Assignment4_Q3{
-    public static void main(String[] args) {
-        try {
-            Scanner file = new Scanner(new File("data.txt"));
-            int largest = file.nextInt();
-
-            while(file.hasNextInt()) {
-                int number = file.nextInt();
-
-                if(number > largest) {
-                    largest = number;
-                }
-
-                System.out.println(number);
-            }
-
-            file.close();
-
-            System.out.println("The biggest number in the file is: " + largest);
-        } catch(IOException e) {
-            System.out.println(e.getMessage());
-        }
-    }
+		
+}
 }
